6f6fc598856ea62ff64c32efde810116fe024285
Sarah <sc705147@gmail.com>
2024-10-03 11:49:43 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+// ação, comédia, drama, romance
+
+// homem aranha 3, +12, ação, aventura
+// meu malvado favorito 4, LIVRE, fantasia, aventura, comédia
+// como eu antes de você,+12, drama, romance
+// deadpool, +16, ação,comédia
+// o auto da compadecida, +12, comédia, aventura
+// fala sério,mãe, +12, comédia,
+// por lugares incríveis, +16, drama,romance
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
